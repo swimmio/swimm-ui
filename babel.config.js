0ea7c7e6fcdf3dec355f90868ebdf2f72915bf43
omerfishman <omerfishman.work@gmail.com>
2021-04-29 17:51:44 +0300

    Removed rollup

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,5 @@
-const devPresets = ['@vue/babel-preset-app'];
-const buildPresets = [
-  [
-    '@babel/preset-env',
-    // Config for @babel/preset-env
-    {
-      // Example: Always transpile optional chaining/nullish coalescing
-      // include: [
-      //   /(optional-chaining|nullish-coalescing)/
-      // ],
-    },
-  ],
-];
 module.exports = {
-  presets: (process.env.NODE_ENV === 'development' ? devPresets : buildPresets),
-};
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
