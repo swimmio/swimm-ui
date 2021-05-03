5c91f1996606af4426c5e5dbbe2394fd9db2c256
omerfishman <omerfishman.work@gmail.com>
2021-05-03 15:07:27 +0300

    Changed exporting option

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,24 @@
-export { default as Action } from '../components/Action/Action';
-export { default as Icon } from '../components/Action/Action';
+import Vue from 'vue';
+
+const req = require.context('../components/', true, /\.(vue)$/i);
+
+const fileIgnores = ['../components/Icon.vue']
+
+const filterIgnores = (file) => {
+    console.log('File Imported : ', file);
+    return fileIgnores.indexOf(file) !== -1;
+}
+
+req.keys().filter(filterIgnores).forEach((fileName) => {
+    // Get component config
+    const componentConfig = req(fileName);
+    const componentName = fileName.match(/\w+/)[0];
+    // Register component globally
+    Vue.component(
+        componentName,
+        // Look for the component options on `.default`, which will
+        // exist if the component was exported with `export default`,
+        // otherwise fall back to module's root.
+        componentConfig.default || componentConfig
+    );
+});
