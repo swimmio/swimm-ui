0c175b6386fd017529a0ed2354b5cf6371d4e4c4
Tal Koren <tal@swimm.io>
2022-02-27 23:50:52 +0200

    docs(vuepress): add vuepress

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,8 @@
+import VTooltip from 'v-tooltip';
+import 'fontaweswimm/src/fonts/fontaweswimm/style.css';
+import '../../public/styles/index.css';
+import '../../src/lib-components';
+
+export default ({ Vue }) => {
+  Vue.use(VTooltip);
+};
---
+++ docs/index.md
@@ -0,0 +1,1 @@
+# Swimm UI
