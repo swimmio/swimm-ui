b3b6243c658af86121158678eeaccfb37b028d13
Tal Koren <tal@swimm.io>
2022-02-27 23:44:49 +0200

    test: add testing

---
+++ src/components/Action/Action.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import Action from "@/components/Action/Action";
+
+describe("Action", () => {
+  it("renders default button", () => {
+    const wrapper = shallowMount(Action);
+    expect(wrapper.text()).toMatch("Button text");
+  });
+});
