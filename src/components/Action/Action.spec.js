f12b4b4412980340e7a574aec570f40945cadaf5
Tal Koren <tal@swimm.io>
2022-02-28 14:41:35 +0200

    style: prettier

--- src/components/Action/Action.spec.js
+++ src/components/Action/Action.spec.js
@@ -1,9 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import Action from "@/components/Action/Action";
+import { shallowMount } from '@vue/test-utils';
+import Action from '@/components/Action/Action';
 
-describe("Action", () => {
-  it("renders default button", () => {
+describe('Action', () => {
+  it('renders default button', () => {
     const wrapper = shallowMount(Action);
-    expect(wrapper.text()).toMatch("Button text");
+    expect(wrapper.text()).toMatch('Button text');
   });
 });
