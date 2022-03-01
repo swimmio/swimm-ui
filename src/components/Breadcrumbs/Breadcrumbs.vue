1598a42928a372e90b313b679c49daf7f5e1f9e7
David Meir-Levy <davidmeirlevy@gmail.com>
2022-03-01 15:45:46 +0200

    fix indentation

--- src/components/Breadcrumbs/Breadcrumbs.vue
+++ src/components/Breadcrumbs/Breadcrumbs.vue
@@ -1,41 +1,41 @@
 <template>
-	<div class='breadcrumbs'>
-		<Breadcrumb v-for='(item, index) in items'
-								:key='index'
-								class='item'
-								:name='item.name'
-								:icon='item.icon'
-								:link='item.link'
-		/>
-	</div>
+  <div class='breadcrumbs'>
+    <Breadcrumb v-for='(item, index) in items'
+                :key='index'
+                class='item'
+                :name='item.name'
+                :icon='item.icon'
+                :link='item.link'
+    />
+  </div>
 </template>
 
 <script>
 import Breadcrumb from './Breadcrumb/Breadcrumb.vue';
 
 export default {
-	components: { Breadcrumb },
-	props: {
-		items: {
-			type: Array,
-			required: true,
-			validator: (items) => {
-				return items.every(({ icon, name }) => {
-					return icon && name;
-				});
-			}
-		}
-	}
+  components: { Breadcrumb },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      validator: (items) => {
+        return items.every(({ icon, name }) => {
+          return icon && name;
+        });
+      }
+    }
+  }
 };
 </script>
 <style scoped>
 .breadcrumbs {
-	display: flex;
-	flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 }
 
 .item:not(:last-child):after {
-		content: '/';
-		padding: 10px;
+  content: '/';
+  padding: 10px;
 }
 </style>
