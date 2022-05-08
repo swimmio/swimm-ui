0c58d716c03d569e72ce05a3fc7856be37907030
Tal Koren <tal@swimm.io>
2022-05-08 13:02:05 +0300

    feat: upgrade to vite and vue3

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  rules: {
+    // TODO: remove these rules after fixing the components
+    'vue/multi-word-component-names': 'warn',
+    'vue/no-mutating-props': 'warn',
+  },
+};
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
