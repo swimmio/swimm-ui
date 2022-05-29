6638e4d5d2d818dead90c34dba07319019a45544
Tal Koren <tal@swimm.io>
2022-05-29 22:02:20 +0300

    ci(commitlint): fix ignore regex

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,6 +4,6 @@ module.exports = {
     'subject-case': [0, 'always', 'lower-case'],
   },
   ignores: [
-    (message) => /^chore\(release\): set `package.json` to.*?$/.test(message),
+    (message) => /^chore\(release\): set `package.json` to.*?$/m.test(message),
   ],
 };
