26be564d8a0b710207627f4882c61c6177b3fc7b
Tal Koren <tal@swimm.io>
2022-06-26 12:37:16 +0300

    chore: add 'next' branch to semantic release

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
-  branches: ['main'],
+  branches: [
+    { name: 'main' },
+    { name: 'next', channel: 'beta', prerelease: 'beta' },
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
