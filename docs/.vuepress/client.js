73b764dd1162a444552096bf53123f921bac89fd
Tal Koren <tal@swimm.io>
2022-06-30 11:08:02 +0300

    docs: fix register tooltip in vuepress

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -2,7 +2,12 @@ import { defineClientConfig } from '@vuepress/client';
 import 'fontaweswimm/src/fonts/fontaweswimm/style.css';
 import '../../public/styles/index.css';
 import FloatingVue from 'floating-vue';
+import 'floating-vue/dist/style.css';
 
-export default defineClientConfig(({ app }) => {
-  app.use(FloatingVue);
+export default defineClientConfig({
+  enhance({ app }) {
+    app.use(FloatingVue, {
+      boundary: 'window',
+    });
+  },
 });
