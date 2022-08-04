8447a567d6a1605700c50017c3a13e30141aed8e
omerfishman <omerfishman.work@gmail.com>
2022-08-04 14:59:06 -0400

    fix(typography): Rewrite SwText in script setup

--- src/components/Typography/SwText.vue
+++ src/components/Typography/SwText.vue
@@ -1,3 +1,55 @@
+<script setup>
+import { computed, defineProps } from 'vue';
+import {
+  FONT_FAMILY,
+  VARIANTS,
+  WEIGHTS,
+  variantToComponentMap,
+  variantToFontFamilyMap,
+  variantToWeightMap,
+} from './constants';
+
+const props = defineProps({
+  component: { type: String, default: null },
+  variant: {
+    type: String,
+    default: VARIANTS.BODY_L,
+    validator: (value) => Object.values(VARIANTS).includes(value),
+  },
+  family: {
+    type: String,
+    default: null,
+    validator: (value) => Object.values(FONT_FAMILY).includes(value),
+  },
+  weight: {
+    type: String,
+    default: null,
+    validator: (value) => Object.values(WEIGHTS).includes(value),
+  },
+});
+
+const viewComponent = computed(() => {
+  if (props.component) {
+    return props.component;
+  }
+
+  return variantToComponentMap[props.variant];
+});
+const fontFamily = computed(() => {
+  if (props.family) {
+    return props.family;
+  }
+
+  return variantToFontFamilyMap[props.variant];
+});
+const fontWeight = computed(() => {
+  if (props.weight) {
+    props.weight;
+  }
+  return String(variantToWeightMap[props.variant]);
+});
+</script>
+
 <template>
   <component
     :is="viewComponent"
@@ -10,63 +62,6 @@
     <slot></slot>
   </component>
 </template>
-
-<script>
-import {
-  FONT_FAMILY,
-  VARIANTS,
-  WEIGHTS,
-  variantToComponentMap,
-  variantToFontFamilyMap,
-  variantToWeightMap,
-} from './constants';
-
-export default {
-  props: {
-    component: { type: String, default: null },
-    variant: {
-      type: String,
-      default: VARIANTS.BODY_L,
-      validator: (value) => Object.values(VARIANTS).includes(value),
-    },
-    family: {
-      type: String,
-      default: null,
-      validator: (value) => Object.values(FONT_FAMILY).includes(value),
-    },
-    weight: {
-      type: String,
-      default: null,
-      validator: (value) => Object.values(WEIGHTS).includes(value),
-    },
-  },
-  setup() {
-    return { VARIANTS };
-  },
-  computed: {
-    viewComponent() {
-      if (this.component) {
-        return this.component;
-      }
-
-      return variantToComponentMap[this.variant];
-    },
-    fontFamily() {
-      if (this.family) {
-        return this.family;
-      }
-
-      return variantToFontFamilyMap[this.variant];
-    },
-    fontWeight() {
-      if (this.weight) {
-        this.weight;
-      }
-      return String(variantToWeightMap[this.variant]);
-    },
-  },
-};
-</script>
 
 <style scoped>
 .some {
