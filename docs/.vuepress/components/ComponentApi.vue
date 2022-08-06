89b2ebe0bf360e6af922184403a34bdbf60414bc
Tal Koren <tal@swimm.io>
2022-08-06 15:01:16 +0300

    docs: generate component api data

---
+++ docs/.vuepress/components/ComponentApi.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { apiData } from '@temp/componentsApi';
+import ComponentProps from './ComponentProps.vue';
+import ComponentEvents from './ComponentEvents.vue';
+import ComponentSlots from './ComponentSlots.vue';
+
+const props = defineProps({
+  name: { type: String, require: true },
+});
+
+const data = apiData.filter(
+  (component) => component.displayName === props.name
+)[0];
+</script>
+
+<template>
+  <ComponentProps v-if="data.props" :component-props="data.props" />
+  <ComponentEvents v-if="data.events" :component-events="data.events" />
+  <ComponentSlots v-if="data.slots" :component-slots="data.slots" />
+</template>
---
+++ docs/.vuepress/components/ComponentProps.vue
@@ -0,0 +1,38 @@
+<script setup>
+const props = defineProps({
+  componentProps: { type: Object, require: true },
+});
+</script>
+
+<template>
+  <h3>Props</h3>
+  <table v-if="componentProps">
+    <thead>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Type</th>
+      <th>Default</th>
+    </thead>
+    <tbody>
+      <tr
+        v-for="{
+          name,
+          type,
+          description,
+          required,
+          defaultValue,
+        } in componentProps"
+      >
+        <td>
+          <strong>{{ name }}</strong>
+        </td>
+        <td>{{ description }}</td>
+        <td>{{ type.name }}</td>
+        <td v-if="required"></td>
+        <td v-else>
+          {{ defaultValue?.value !== 'null' ? defaultValue.value : '' }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
