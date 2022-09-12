d49ceb1879f5171ccb03d4f2d9bbcbc6227d6508
YossiSaadi <yossi@swimm.io>
2022-09-12 14:39:48 +0300

    feat(TextSkeleton): add component for loading state skeletons

---
+++ docs/components/text-skeleton.md
@@ -0,0 +1,21 @@
+<script setup>
+import { TextSkeleton } from '@swimm/ui';
+</script>
+
+# TextSkeleton
+
+### Title with width
+
+::: demo
+<TextSkeleton variant="title" width="300"/>
+:::
+
+### Body-L
+
+::: demo
+<TextSkeleton variant="body-L" />
+:::
+
+## API
+
+<ComponentApi name="TextSkeleton" />
---
+++ src/components/TextSkeleton/TextSkeleton.vue
@@ -0,0 +1,25 @@
+<script setup>
+import SwText from '../Typography/SwText.vue';
+
+defineProps({
+  variant: { type: String, required: true },
+  width: { type: String, default: '100' },
+});
+</script>
+
+<template>
+  <SwText :variant="variant" class="skeleton" />
+</template>
+
+<style scoped>
+.skeleton {
+  background: var(--color-surface);
+  margin-top: var(--space-xs);
+  border-radius: 4px;
+  width: calc(v-bind(width) * 1px);
+}
+
+.skeleton:not(h1, h2, h3, h4, h5, h6):before {
+  content: '\200b'; /* to apply height for empty div/span */
+}
+</style>
