786051d5f26d348780ab243dd4025fb08fdf56d1
Omer Fishman <omerfishman.work@gmail.com>
2022-11-16 10:51:22 -0500

    Update src/components/Action/constants.ts
    Co-authored-by: Segev Finer <segev208@gmail.com>

--- src/components/Action/constants.ts
+++ src/components/Action/constants.ts
@@ -2,10 +2,12 @@ export enum SIZES {
   BIG = 'big',
   SMALL = 'small',
 }
+
 export enum VARIANTS {
   DANGER = 'danger',
   SUCCESS = 'success',
 }
+
 export enum TYPES {
   BUTTON = 'button',
   LINK = 'a',
