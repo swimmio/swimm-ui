a041cf03cf1a208e1a81e4fb34859e9e4f6258a8
Eran Keydar <eran@swimm.io>
2023-01-26 12:34:03 +0200

    feat(Loader): add small variant

--- src/components/Loader/Loader.vue
+++ src/components/Loader/Loader.vue
@@ -5,6 +5,7 @@ const hide = ref(false);
 const props = defineProps({
   noFlickering: { type: Boolean, default: false },
   secondary: { type: Boolean, default: false },
+  small: { type: Boolean, default: false },
 });
 
 onMounted(() => {
@@ -16,12 +17,15 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="loader" :class="{ hide: hide, secondary }"></div>
+  <div class="loader" :class="{ hide: hide, secondary, small }"></div>
 </template>
 
 <style scoped>
 .loader {
-  --loader-size: 150px;
+  --loader-size-primary: 150px;
+  --loader-size-secondary: 70px;
+  --loader-size-small: 30px;
+  --loader-size: var(--loader-size-primary);
   background-image: url('@/assets/loading.gif');
   background-size: var(--loader-size);
   background-repeat: no-repeat;
@@ -30,10 +34,20 @@ onMounted(() => {
   min-height: var(--loader-size);
   min-width: var(--loader-size);
 }
+
 .loader.secondary {
-  --loader-size: 70px;
+  --loader-size: var(--loader-size-secondary);
   background-image: url('@/assets/secondary-loading.gif');
 }
+
+.loader.small {
+  --loader-size: var(--loader-size-small);
+}
+
+.loader.secondary.small {
+  --loader-size: var(--loader-size-small);
+}
+
 .loader.hide {
   visibility: hidden;
 }
